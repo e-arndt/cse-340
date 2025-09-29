0c099a636244b1fd18bd33a8481db8f961b62d86
Eric Arndt <earndt1701@gmail.com>
2025-09-29 10:02:15 -0700

    Update

--- database/index.js
+++ database/index.js
@@ -1,37 +1,38 @@
 const { Pool } = require("pg")
 require("dotenv").config()
+
 /* ***************
  * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
+ * Render requires SSL in production
+ * Local dev does not
  * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
+
+if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // 👈 required on Render
     },
-})
+  })
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+}
 
-// Added for troubleshooting queries
-// during development
+// Added for troubleshooting queries during development
 module.exports = {
   async query(text, params) {
     try {
       const res = await pool.query(text, params)
-      console.log("executed query", { text })
+      if (process.env.NODE_ENV !== "production") {
+        console.log("executed query", { text })
+      }
       return res
     } catch (error) {
       console.error("error in query", { text })
       throw error
     }
   },
-}
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
 }
