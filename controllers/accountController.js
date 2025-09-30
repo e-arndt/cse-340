d5b1070a5ddf66c341d46dceea1b81966825669b
Eric Arndt <earndt1701@gmail.com>
2025-09-29 18:11:18 -0700

    Login Process, so far

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,8 @@
 const utilities = require("../utilities")
 const accountModel = require("../models/account-model")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 
 /* ****************************************
@@ -94,5 +96,72 @@ async function registerAccount(req, res, next) {
 }
 
 
-module.exports = { buildLogin, buildRegister, registerAccount }
+/* ****************************************
+ *  Process login request
+ * ************************************ */
+async function accountLogin(req, res) {
+  const nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
+
+  // If no account found
+  if (!accountData) {
+    req.flash("notice", "Please check your credentials and try again.")
+    return res.status(400).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+      account_email,
+    })
+  }
+
+  try {
+    // Compare entered password with hashed password in DB
+    const passwordMatch = await bcrypt.compare(account_password, accountData.account_password)
+
+    if (passwordMatch) {
+      delete accountData.account_password
+
+      // Create JWT token (expires in 1 hour = 3600 seconds)
+      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 })
+
+      // Set cookie with JWT token
+      if (process.env.NODE_ENV === "development") {
+        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+      } else {
+        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+      }
+
+      // Redirect to account management area
+      return res.redirect("/account/")
+    } else {
+      // Invalid password
+      req.flash("notice", "Please check your credentials and try again.")
+      return res.status(400).render("account/login", {
+        title: "Login",
+        nav,
+        errors: null,
+        account_email,
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    req.flash("notice", "Something went wrong. Please try again.")
+    return res.status(500).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+      account_email,
+    })
+  }
+}
+
+
+module.exports = { 
+  buildLogin, 
+  buildRegister, 
+  registerAccount, 
+  accountLogin 
+}
+
 
